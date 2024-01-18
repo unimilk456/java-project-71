78ecc9c831b50a672fec95653dc6cabd98023995
Mikhail-Davidovich <mikhail.davidovich@edetek.com>
2024-01-18 15:42:33 +0300

    Finished stage 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,11 +2,23 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
+    String format = "stylish";
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
     public static void main(String[] args) {
         System.out.println("Hello World!");
         int exitCode = new CommandLine(new App()).execute(args);
@@ -17,4 +29,5 @@ public class App implements Callable<Integer> {
     public Integer call(){
         return null;
     }
+    // Custom parameter renderer to exclude angle brackets
 }
