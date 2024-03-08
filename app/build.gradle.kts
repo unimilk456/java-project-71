113f0b35bf044893af2d999a266400831346381a
Mikhail-Davidovich <mikhail.davidovich@edetek.com>
2024-03-08 14:29:53 +0400

    Finished stage 5 ( generated diff )

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,15 @@ repositories {
 
 dependencies {
     implementation ("info.picocli:picocli:4.7.5")
+    implementation ("commons-cli:commons-cli:1.4")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.16.0")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("org.slf4j:slf4j-log4j12:2.0.12")
+
+//    implementation ("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,33 +1,43 @@
 package hexlet.code;
 
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
-
-    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
-    String format = "stylish";
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    String format;
 
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filePath1;
 
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
     private String filePath2;
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int exitCode = new CommandLine(new App()).execute(args);
-//        System.exit(exitCode);
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.out.println("Please provide two file paths as arguments");
+            return;
+        }
+        Path path1 = Paths.get(args[0]);
+        Path path2 = Paths.get(args[1]);
+
+        String result = Differ.generate(path1, path2);
+
+        System.out.println(result);
     }
 
     @Override
-    public Integer call(){
+    public Integer call() {
         return null;
     }
-    // Custom parameter renderer to exclude angle brackets
+
 }
