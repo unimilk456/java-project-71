0bb87a5b449b80d6aa321a75ae072a57f99332eb
unimilk456 <unimilk456@gmail.com>
2024-03-08 16:58:02 +0400

    small fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.12")
     implementation("org.slf4j:slf4j-log4j12:2.0.12")
 
-//    implementation ("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
